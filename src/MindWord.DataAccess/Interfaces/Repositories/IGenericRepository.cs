2136ba60afe36ba0bd0f4e1d5177535a97c0a746
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2022-11-22 18:56:44 +0500

    IUserRepository and UserRepository added

---
+++ src/MindWord.DataAccess/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindWord.DataAccess.Interfaces.Repositories
+{
+    public interface IGenericRepository<T>
+    {
+        Task<bool> CreateAsync(T item);
+        Task<bool> UpdateAsync(int id, T entity);
+        Task<bool> DeleteAsync(int id);
+        Task<T> GetByIdAsync(int id);
+        Task<IList<T>> GetAllAsync(int skip, int take);
+    }
+}
--- src/MindWord.Domain/Common/BaseEntity.cs
+++ src/MindWord.Domain/Common/BaseEntity.cs
@@ -2,5 +2,5 @@
 
 public class BaseEntity
 {
-    public long Id { get; set; }
+    public int Id { get; set; }
 }
