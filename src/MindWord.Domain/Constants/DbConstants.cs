52ba023d1d949d55c2382b33132d1057cf191b0a
Quvondiq <jumaboyevquvondiqm5202@gmail.com>
2022-11-23 21:25:11 +0500

    Update DBConstants and create Category entity

--- src/MindWord.Domain/Constants/DbConstants.cs
+++ src/MindWord.Domain/Constants/DbConstants.cs
@@ -8,6 +8,6 @@ namespace MindWord.Domain.Constants
 {
     public class DbConstants
     {
-        public const string CONNECTION_STRING = "C:/Users/mindword/databases/Mindword.db";
+        public const string CONNECTION_STRING = "../../../../../database/Mindword.db";
     }
 }
