a787ce18b2024bc209bd16a3eef3ea5319d6da30
A S U S <mebeluz555520@gmail.com>
2022-11-23 17:01:05 +0500

    guyni

---
+++ src/MindWord.Domain/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindWord.Domain
+{
+    public class Class2
+    {
+        public string Name { get; set; }
+    }
+}
