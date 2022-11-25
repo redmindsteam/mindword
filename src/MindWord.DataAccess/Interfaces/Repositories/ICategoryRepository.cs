4cf806cda2b18252a888cc4c7787d8176a44ff50
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2022-11-25 23:24:44 +0500

    generic

--- src/MindWord.DataAccess/Interfaces/Repositories/ICategoryRepository.cs
+++ src/MindWord.DataAccess/Interfaces/Repositories/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MindWord.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MindWord.DataAccess.Interfaces.Repositories
 {
-    internal interface ICategoryRepository
+    public interface ICategoryRepository : IGenericRepository<Category>
     {
     }
 }
