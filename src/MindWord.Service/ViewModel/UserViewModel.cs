f2d72bb702bbf4c26fd6e2bff0c7e3aff6a29be8
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2022-11-26 22:40:56 +0500

    delete auditable Quvondiq

--- src/MindWord.Service/ViewModel/UserViewModel.cs
+++ src/MindWord.Service/ViewModel/UserViewModel.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MindWord.Service.ViewModel
 {
-    public class UserViewModel : Auditable
+    public class UserViewModel 
     {
         public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
