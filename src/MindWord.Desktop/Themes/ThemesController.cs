77aa2c97cf9348097805f40476aee6b4278a1da4
Normadjon <gofforovnormadjon@gmail.com>
2022-11-29 21:34:31 +0500

    Button and radio button style create

---
+++ src/MindWord.Desktop/Themes/ThemesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MindWord.Desktop.Themes
+{
+    public class ThemesController
+    {
+        public static ThemeTypes CurrentTheme { get; set; }
+
+        public enum ThemeTypes
+        {
+            Light, Dark
+        }
+
+        public static ResourceDictionary ThemeDictionary
+        {
+            get { return Application.Current.Resources.MergedDictionaries[0]; }
+            set { Application.Current.Resources.MergedDictionaries[0] = value; }
+        }
+
+        private static void ChangeTheme(Uri uri)
+        {
+            ThemeDictionary = new ResourceDictionary() { Source = uri };
+        }
+
+        public static void SetTheme(ThemeTypes theme)
+        {
+            string themeName = "";
+            CurrentTheme = theme;
+            switch (theme)
+            {
+                case ThemeTypes.Dark: themeName = "DarkTheme"; break;
+                case ThemeTypes.Light: themeName = "LightTheme"; break;
+            }
+
+            try
+            {
+                if (!string.IsNullOrEmpty(themeName))
+                    ChangeTheme(new Uri($"Themes/{themeName}.xaml", UriKind.Relative));
+            }
+            catch { }
+        }
+    }
+}
