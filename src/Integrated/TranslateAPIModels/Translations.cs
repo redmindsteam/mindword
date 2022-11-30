15738943e1d9839ee90d6a9b4f32b7c614852472
Qahhor <nurmamatovnodirbek43@gmail.com>
2022-11-30 17:09:22 +0500

    Translate created

---
+++ src/Integrated/TranslateAPIModels/Translations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrated.TranslateAPIModels
+{
+    public class Translations
+    {
+        public string Text { get; set; } = string.Empty;
+        public string ToLanguage { get; set; } = string.Empty;
+    }
+}
