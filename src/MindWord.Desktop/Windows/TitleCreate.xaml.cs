c37ed590d475e75a069acea725ffd40e50af4c70
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2022-12-01 17:27:15 +0500

    CategoryService create

--- src/MindWord.Desktop/Windows/TitleCreate.xaml.cs
+++ src/MindWord.Desktop/Windows/TitleCreate.xaml.cs
@@ -23,5 +23,10 @@ namespace MindWord.Desktop.Windows
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
