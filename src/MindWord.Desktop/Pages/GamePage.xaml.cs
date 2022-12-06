6bee55feadd47829dfdb92316443f7854987a5dc
Normadjon <gofforovnormadjon@gmail.com>
2022-12-06 09:02:14 +0500

    Game page create

--- src/MindWord.Desktop/Pages/GamePage.xaml.cs
+++ src/MindWord.Desktop/Pages/GamePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using MindWord.Desktop.Windows;
+using System.Windows.Controls;
 
 namespace MindWord.Desktop.Pages
 {
@@ -10,6 +11,30 @@ namespace MindWord.Desktop.Pages
         public GamePage()
         {
             InitializeComponent();
+        }
+
+        private void btnWRandom_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            WRandomWindow wRandomWindow = new WRandomWindow();
+            wRandomWindow.Show();
+        }
+
+        private void btnWVoice_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            WVoiceWindow wVoiceWindow = new WVoiceWindow();
+            wVoiceWindow.Show();
+        }
+
+        private void btnWDescription_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            WDescriptionWindow wDescriptionWindow = new WDescriptionWindow();
+            wDescriptionWindow.Show();
+        }
+
+        private void btnWTranslate_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            WTranslateWindow wTranslateWindow = new WTranslateWindow(); 
+            wTranslateWindow.Show();
         }
     }
 }
