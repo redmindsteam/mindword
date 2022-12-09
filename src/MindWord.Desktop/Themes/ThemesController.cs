ed22aa78af0f2f3de4e691bc186cfe2324dc5533
Normadjon <gofforovnormadjon@gmail.com>
2022-12-10 01:58:04 +0500

    Theme update

--- src/MindWord.Desktop/Themes/ThemesController.cs
+++ src/MindWord.Desktop/Themes/ThemesController.cs
@@ -19,9 +19,16 @@ namespace MindWord.Desktop.Themes
             set { Application.Current.Resources.MergedDictionaries[0] = value; }
         }
 
-        private static void ChangeTheme(Uri uri)
+        public static ResourceDictionary MaterialDictionary
+        {
+            get { return Application.Current.Resources.MergedDictionaries[2]; }
+            set { Application.Current.Resources.MergedDictionaries[2] = value; }
+        }
+
+        private static void ChangeTheme(Uri uri, Uri uri2)
         {
             ThemeDictionary.Source = uri ;
+            MaterialDictionary.Source = uri2;
         }
 
         public static void SetTheme(ThemeTypes theme)
@@ -30,16 +37,18 @@ namespace MindWord.Desktop.Themes
             CurrentTheme = theme;
             switch (theme)
             {
-                case ThemeTypes.Dark: themeName = "DarkTheme"; break;
-                case ThemeTypes.Light: themeName = "LightTheme"; break;
+                case ThemeTypes.Dark: themeName = "Dark"; break;
+                case ThemeTypes.Light: themeName = "Light"; break;
             }
 
             try
             {
                 if (!string.IsNullOrEmpty(themeName))
                 {
-                    var str = new FileInfo($"../../../Themes/{themeName}.xaml").FullName;
-                    ChangeTheme(new Uri(str));
+                    var str = new FileInfo($"../../../Themes/{themeName}Theme.xaml").FullName;
+                    var str2 = new FileInfo($"/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.{themeName}.xaml").FullName;
+                    ChangeTheme(new Uri(str), new Uri(str2));
+
                 }
             }
             catch { }
