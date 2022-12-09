94e2d5960a0bb3c0eb63ffe3fae7b88b745cf5c3
Normadjon <gofforovnormadjon@gmail.com>
2022-12-09 11:31:32 +0500

    Result game window create

---
+++ src/MindWord.Desktop/Windows/GameResultWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MindWord.Desktop.Windows
+{
+    /// <summary>
+    /// Interaction logic for GameResultWindow.xaml
+    /// </summary>
+    public partial class GameResultWindow : Window
+    {
+        public GameResultWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBox_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnInfo_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dgData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnLeft_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRight_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pageSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private async void tbSearchBox_TextChanged(string txt)
+        {
+
+        }
+    }
+}
