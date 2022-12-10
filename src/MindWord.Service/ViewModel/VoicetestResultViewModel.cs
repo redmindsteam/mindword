40b2369e8b099c25ae3c2a102ba8568474e2d985
JumaboyevQuvondiq <jumaboyevquvondiqm5202@gmail.com>
2022-12-10 14:53:47 +0500

    Update voice Result

---
+++ src/MindWord.Service/ViewModel/VoicetestResultViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MindWord.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindWord.Service.ViewModel
+{
+    public class VoicetestResultViewModel:BaseEntity
+    {
+        public string AudioPath { get; set; } = string.Empty;
+        public string Translate { get; set; } = string.Empty;
+        public string Answer { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;      
+    }
+}
